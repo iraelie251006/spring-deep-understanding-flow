6f6d02402a1be79b0de56885f9c4adfd4b30f16e
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-15 17:05:14 +0200

    Added missing imports

--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -1,5 +1,13 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter @Setter @NoArgsConstructor
 public class Customer {
