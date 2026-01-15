32c7d3100538370fa5d220b80820c30134e31303
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-15 17:16:12 +0200

    Implemented Invoice model

--- src/main/java/com/example/demo/model/Invoice.java
+++ src/main/java/com/example/demo/model/Invoice.java
@@ -1,4 +1,24 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal price;
+    private BigDecimal tax;
+    private BigDecimal total;
 }
