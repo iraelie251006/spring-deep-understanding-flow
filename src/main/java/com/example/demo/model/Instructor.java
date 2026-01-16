2452d47284e5347c6332578497878e1b7b38c6ab
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-16 22:36:16 +0200

    Established bidirectional relationship between Instructor and courses

--- src/main/java/com/example/demo/model/Instructor.java
+++ src/main/java/com/example/demo/model/Instructor.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Setter
@@ -18,6 +19,12 @@ public class Instructor {
     private String name;
     private String email;
 
-    @OneToMany(mappedBy = "instructor", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "instructor", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Course> courses;
+
+    public void addCourses(Course course) {
+        if (courses == null) courses = new ArrayList<>();
+        courses.add(course);
+        course.setInstructor(this);
+    }
 }
