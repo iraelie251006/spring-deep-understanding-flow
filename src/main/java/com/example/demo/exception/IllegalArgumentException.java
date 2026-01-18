6fd5cda0d93a154fa380422978cf44002ad69618
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-18 23:34:14 +0200

    Addded a comment

--- src/main/java/com/example/demo/exception/IllegalArgumentException.java
+++ src/main/java/com/example/demo/exception/IllegalArgumentException.java
@@ -1,6 +1,8 @@
 package com.example.demo.exception;
 
 public class IllegalArgumentException extends BaseException{
+
+//    IllegalArgumentException TO CATCH illegal cases.
     public IllegalArgumentException(String message) {
         super(message);
     }
