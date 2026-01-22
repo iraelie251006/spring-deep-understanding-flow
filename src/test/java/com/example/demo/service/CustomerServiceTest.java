6d377fa48c878121fa92f231f0e523049c330a03
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-22 13:45:17 +0200

    Created Mocks and injected mocks into customer service.

--- src/test/java/com/example/demo/service/CustomerServiceTest.java
+++ src/test/java/com/example/demo/service/CustomerServiceTest.java
@@ -1,9 +1,26 @@
 package com.example.demo.service;
 
+import com.example.demo.mapper.CustomerMapper;
+import com.example.demo.repository.CustomerRepository;
+import com.example.demo.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
+
+    @Mock
+    private CustomerRepository customerRepository;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private CustomerMapper customerMapper;
+
+    @InjectMocks
+    private CustomerService customerService;
 
     @Test
     void getCustomer() {
