1589d9016ca5034566efa7cc7ff66e6c6be401e7
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-22 13:42:09 +0200

    Created Customer service test

---
+++ src/test/java/com/example/demo/service/CustomerServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+
+class CustomerServiceTest {
+
+    @Test
+    void getCustomer() {
+    }
+
+    @Test
+    void createOrder() {
+    }
+}
