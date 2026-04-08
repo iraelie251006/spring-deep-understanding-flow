16ddba63f3fc851a894386dac1a5cc620d768ee6
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-08 23:23:57 +0200

    implemented transfer method

--- src/main/java/com/example/demo/patterns/threada/BankAccount.java
+++ src/main/java/com/example/demo/patterns/threada/BankAccount.java
@@ -3,4 +3,7 @@ package com.example.demo.patterns.threada;
 class BankAccount {
     private double balance;
     private final Object lock = new Object();
+    public void transfer(BankAccount to, double amt) throws InterruptedException {
+    }
 }
+
